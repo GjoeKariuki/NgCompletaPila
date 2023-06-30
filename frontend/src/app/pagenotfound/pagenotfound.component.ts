7fa71366805a27f4e924ffe723254ecbbb9749a8
GjoeKariuki <githaigageorge96@gmail.com>
2023-06-30 11:31:47 +0300

    June30th

--- frontend/src/app/pagenotfound/pagenotfound.component.ts
+++ frontend/src/app/pagenotfound/pagenotfound.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -9,5 +10,10 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./pagenotfound.component.css']
 })
 export class PagenotfoundComponent {
+  constructor(private  router:Router, private route:ActivatedRoute) {
 
+  }
+  goBack(){
+    this.router.navigate(['../'],{relativeTo:this.route})
+  }
 }
